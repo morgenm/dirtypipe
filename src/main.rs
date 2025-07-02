75717dd7a8a88d90abcce8acbaa8281ab296106d
Morgen Malinoski <51513125+morgenm@users.noreply.github.com>
2025-07-02 14:38:34 -0500

    Kernel version check.

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,47 @@ struct Opt {
     offset: Option<i64>,
 }
 
+// Check if the host's kernel version to see if it is vulnerable.
+// Basing version check on https://securitylabs.datadoghq.com/articles/dirty-pipe-vulnerability-overview-and-remediation/
+fn is_kernel_vuln() -> Result<bool, anyhow::Error> {
+    // Read version string from /proc/version
+    let mut proc_version = fs::read_to_string("/proc/version")?;
+    
+    // Split to get just the kernel version
+    let release_strings = proc_version.split("Linux version ").collect::<Vec<&str>>()[1].split(" ").collect::<Vec<&str>>()[0].split("-").collect::<Vec<&str>>();
+    let kernel_version = String::from(release_strings[0]) + "-" + release_strings[1];
+
+    println!("Kernel version: {}", kernel_version);
+
+    // Split version string to check major and minor versions
+    let versions: Vec<String> = kernel_version.split(".").map(|x| String::from(x)).collect();
+    if versions[0].parse::<i32>().unwrap() != 5 { // Must be Kernel 5.x
+        return Ok(false);
+    }
+    if versions[1].parse::<i32>().unwrap() < 8 || versions[1].parse::<i32>().unwrap() > 16 {
+        return Ok(false)
+    }
+
+    // Check patch level
+    let patch_level = versions[2].split("-").collect::<Vec<&str>>()[0].parse::<i32>().unwrap();
+    let is_vuln = match versions[1].parse::<i32>().unwrap() {
+        16 => patch_level < 11,
+        15 => patch_level < 25,
+        10 => patch_level < 102,
+        _ => true
+    };
+    Ok(is_vuln)
+}
+
 fn main() -> Result<(), anyhow::Error> {
+    // Warn if not a vulnerable kernel version
+    if !is_kernel_vuln()? {
+        println!("Warning! Your kernel version does not appear vulnerable!")
+    }
+    else {
+        println!("Your kernel appears to be vulnerable.")
+    }
+    
     let opt = Opt::from_args();
 
     match opt.mode.as_str() {
